c4d3e5eadc9738ac2afeb8de8b9e0b7e30f3536e
ajalingog <alingogaj2@gmail.com>
2026-01-19 21:11:09 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/alingog-portfolio/', 
+  base: '/alingog-portfolio/', // must match your repo name exactly
 })
